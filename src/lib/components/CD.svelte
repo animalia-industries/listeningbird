e70d7ba20fc46b2e9f1e794eeeffed8f54754d19
CanarDev <vincentmichelnavarro@gmail.com>
2024-06-06 11:24:44 +0200

    :lipstick: Update UI design

---
+++ src/lib/components/CD.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    // Set default artwork vynil image
+    export let artworkURL: string = 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg';
+    export let isWrapped: boolean = false;
+
+</script>
+
+<div class="rounded-full aspect-square flex items-center justify-center relative {$$props.class} {isWrapped ? '' : 'overflow-hidden'} {isWrapped ? '' : 'animate-spin-slow'}">
+    <img src="/images/vinyl-layout.png" alt="Vinyl" class="w-full h-full object-cover opacity-30 absolute z-50" />
+    <img src="{artworkURL}" alt="Spinner" class="object-cover w-full h-full " />
+    {#if !isWrapped}
+        <div class="rounded-full bg-neutral-800 h-1/6 w-1/6 absolute">
+            <div class="rounded-full bg-neutral-300/50 h-1/6 w-1/6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+        </div>
+    {/if}
+    {#if isWrapped}
+        <img class="absolute scale-110" src="/images/cover-layout.png" alt="">
+    {/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import CD from "$lib/components/CD.svelte";
+</script>
+
+<!-- This will be the search page -->
+
+
+<div class="grow flex flex-col-reverse md:flex-row gap-x-2">
+    <div class="my-auto">
+        <CD class="w-3/4" isWrapped artworkURL="https://store.twentyonepilots.com/cdn/shop/files/TOP_CL5_Cover_mailer_1.jpg?v=1709152879"  />
+    </div>
+    <div class="flex flex-col items-center md:items-start justify-center z-50 w-full md:self-end md:my-auto">
+        <div class="flex flex-col w-full">
+            <h1 class="text-white font-koulen text-4xl">OUI</h1>
+            <p class="text-xl font-koulen text-white mb-4">Search any song, artist or album</p>
+            <input type="text" class="font-koulen w-full p-2 bg-neutral-800 text-white" placeholder="Search for an album" />
+        </div>
+    </div>
+</div>
