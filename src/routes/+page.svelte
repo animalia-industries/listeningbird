3426a49645052f09f61b3ee65d943531a288db5f
CanarDev <vincentmichelnavarro@gmail.com>
2024-06-06 11:28:36 +0200

    :lipstick: Update responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,14 @@
 <!-- This will be the search page -->
 
 
-<div class="grow flex flex-col-reverse md:flex-row gap-x-2">
-    <div class="my-auto">
-        <CD class="w-3/4" isWrapped artworkURL="https://store.twentyonepilots.com/cdn/shop/files/TOP_CL5_Cover_mailer_1.jpg?v=1709152879"  />
+<div class="grow flex flex-col-reverse justify-between md:flex-row gap-x-2">
+    <div class="md:my-auto">
+        <CD class="translate-y-1/3 md:translate-y-0 md:w-3/4" isWrapped artworkURL="https://store.twentyonepilots.com/cdn/shop/files/TOP_CL5_Cover_mailer_1.jpg?v=1709152879"  />
     </div>
-    <div class="flex flex-col items-center md:items-start justify-center z-50 w-full md:self-end md:my-auto">
+    <div class="flex flex-col items-center md:items-start justify-center z-50 w-full md:self-end md:my-auto mt-12">
         <div class="flex flex-col w-full">
-            <h1 class="text-white font-koulen text-4xl">OUI</h1>
-            <p class="text-xl font-koulen text-white mb-4">Search any song, artist or album</p>
-            <input type="text" class="font-koulen w-full p-2 bg-neutral-800 text-white" placeholder="Search for an album" />
+            <p class="text-white font-koulen text-4xl text-center md:text-start mb-4">Search any song, artist or album</p>
+            <input type="text" class="rounded-full font-koulen w-full px-4 py-2 bg-neutral-800 text-white" placeholder="Search for an album" />
         </div>
     </div>
 </div>
