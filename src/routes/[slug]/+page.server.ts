08125c536fa81e26e0e386be04bd60a876b3359b
CanarDev <vincentmichelnavarro@gmail.com>
2024-06-06 14:42:43 +0200

    :sparkles: Single

---
+++ src/routes/[slug]/+page.server.ts
@@ -0,0 +1,17 @@
+import type { PageServerLoad } from '@sveltejs/kit';
+import { dynamoDbClient } from "$lib/aws.server";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+
+
+export const load: PageServerLoad = async ({ params }) => {
+    return {
+        data: await DynamoDBDocument.from(dynamoDbClient).get(
+        {
+                TableName: 'musiclinks',
+                Key: {
+                    id: params.slug
+                }
+            }
+        ),
+    };
+};
