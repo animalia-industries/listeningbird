f843829458969f6ebdd7fcdfdcfcf11fefe1e854
yannisobert <90550556+yannisobert@users.noreply.github.com>
2024-06-06 15:08:33 +0200

    feat: Add amplify

--- src/lib/server/aws.server.ts
+++ src/lib/server/aws.server.ts
@@ -3,17 +3,17 @@ import { S3Client, type S3ClientConfig } from '@aws-sdk/client-s3';
 import { DynamoDBClient, type DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
 
 export const dynamoDbClient = new DynamoDBClient({
-  region: env.AWS_REGION,
+  region: env.CUSTOM_AWS_REGION,
   credentials: {
-    accessKeyId: env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: env.CUSTOM_AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.CUSTOM_AWS_SECRET_ACCESS_KEY
   }
 } as DynamoDBClientConfig);
 
 export const s3Client = new S3Client({
-  region: env.AWS_REGION,
+  region: env.CUSTOM_AWS_REGION,
   credentials: {
-    accessKeyId: env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: env.CUSTOM_AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.CUSTOM_AWS_SECRET_ACCESS_KEY
   }
 } as S3ClientConfig);
