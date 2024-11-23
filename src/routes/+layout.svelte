62ed878522b49333a18319111aff8aec87d91062
Guillaume Fine <90453342+Cosmeak@users.noreply.github.com>
2024-11-23 23:44:49 +0100

    wip: v1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,13 @@
 	<div class="container flex grow flex-col py-4">
 		<div class="z-50 flex w-full items-center justify-between gap-x-2">
 			<a href="/" class="font-koulen text-2xl text-white">Listeningbird</a>
-			<div class="flex gap-x-2">
+			<!-- <div class="flex gap-x-2">
 				<a href="/" class="font-koulen text-white">HOME</a>
-			</div>
+			</div> -->
 		</div>
 		<slot />
 	</div>
+	
 	<!-- Overlay -->
 	<div
 		class="fixed top-0 h-screen w-screen bg-gradient-to-t from-stone-950 to-stone-950/0 to-40% md:bg-gradient-to-r md:to-30%"
--- src/routes/api/generate-links/+server.ts
+++ src/routes/api/generate-links/+server.ts
@@ -7,32 +7,43 @@ import { searchOnDeezer } from '$lib/server/deezer.server';
 export const POST: RequestHandler = async ({ request }) => {
 	const data = await request.json();
 
-	// Get from Spotify
+	// Search on platforms
 	const spotifyResponse = await spotifyClient[data.type + 's'].get(data.id);
-
-	//  Get from Deezer
 	const deezerResponse = await searchOnDeezer(spotifyResponse.name, spotifyResponse.type);
 
-	// Save to DynamoDB
-	const link = await db.links.create({ data: {
-		name: spotifyResponse.name,
-		sourceUrl: spotifyResponse.href,
-		type: spotifyResponse.type,
-		images: JSON.stringify(spotifyResponse.album?.images ?? spotifyResponse.images),
-		spotify: JSON.stringify({
-			id: spotifyResponse.id,
-			url: spotifyResponse.external_urls.spotify
-		}),
-		deezer: JSON.stringify({
-			id: deezerResponse.data[0].id,
-			url: deezerResponse.data[0].link
-		}),
-	}});
+	const existingLink = await db.links.findFirst({
+		where: {
+			type: { equals: spotifyResponse.type },
+			spotify: { contains: spotifyResponse.id }
+		}
+	})
+
+	let link = undefined;
+	if (!existingLink) {
+		link = await db.links.create({
+			data: {
+				name: spotifyResponse.name,
+				sourceUrl: spotifyResponse.href,
+				type: spotifyResponse.type,
+				images: JSON.stringify(spotifyResponse.album?.images ?? spotifyResponse.images),
+				spotify: JSON.stringify({
+					id: spotifyResponse.id,
+					url: spotifyResponse.external_urls.spotify
+				}),
+				deezer: JSON.stringify({
+					id: deezerResponse.data[0].id,
+					url: deezerResponse.data[0].link
+				}),
+			}
+		});
+	}
+
+
 
 	return json({
 		status: 200,
 		data: {
-			id: link.id
+			id: existingLink?.id ?? link.id
 		}
 	});
 };
